44e31160e73c1e0caf7191620c7b41e6d5377bd2
nowarich <nowar@bk.ru>
2020-11-30 20:17:57 +0300

    +task 1

--- src/ru/geekbains/DZ/Main.java
+++ src/ru/geekbains/DZ/Main.java
@@ -7,36 +7,36 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-//        String[] words = {
-//          "Marry",
-//          "had",
-//          "a",
-//          "little",
-//          "lamb",
-//          "little",
-//          "lamb",
-//          "little",
-//          "lamb",
-//          "whose",
-//          "fleece",
-//          "was",
-//          "white",
-//          "as",
-//          "snow",
-//        };
-//
-//        Map<String, Integer> uniqueWords = new HashMap<String, Integer>();
-//        for (String strings : words) {
-//            if (uniqueWords.containsKey(strings)) {
-//                int value = uniqueWords.get(strings);
-//                uniqueWords.put(strings, value+1);
-///////                uniqueWords.values(strings) = uniqueWords.values()+1
-//            }    /////{uniqueWords.replace(strings, value, value+1)} // изящно не получилось
-//            else
-//            uniqueWords.put(strings, 1);
-//
-//        }
-//        System.out.println(String.format("There are %s unique words in an Array: %s", uniqueWords.size(), uniqueWords.toString()));
+        String[] words = {
+          "Marry",
+          "had",
+          "a",
+          "little",
+          "lamb",
+          "little",
+          "lamb",
+          "little",
+          "lamb",
+          "whose",
+          "fleece",
+          "was",
+          "white",
+          "as",
+          "snow",
+        };
+
+        Map<String, Integer> uniqueWords = new HashMap<String, Integer>();
+        for (String strings : words ) {
+            if (uniqueWords.containsKey(strings)) {
+                int value = uniqueWords.get(strings);
+                uniqueWords.put(strings, value+1);
+/////                uniqueWords.values(strings) = uniqueWords.values()+1
+            }    /////{uniqueWords.replace(strings, value, value+1)} // изящно не получилось
+            else
+            uniqueWords.put(strings, 1);
+
+        }
+        System.out.println(String.format("There are %s unique words in an Array: %s", uniqueWords.size(), uniqueWords.toString()));
 ///////////////////////////////////////// TASK 2 /////////////////////////////
 //        Map<String, Long> phoneBook = new HashMap<String, Long>();
 //
